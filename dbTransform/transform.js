7deb68fff30b1ad12978edb825fc14b5b10f0ab0
Will Freeman <will.freeman@hillmangroup.com>
2021-02-17 01:32:31 -0700

    add db cloning script

---
+++ dbTransform/transform.js
@@ -0,0 +1,90 @@
+// get configuration from top-level .env file
+const path = require("path");
+require("dotenv").config({
+    path: path.resolve(__dirname, '../.env')
+});
+
+const fs = require("fs");
+const csv = require("csv");
+const { spawn } = require("child_process");
+
+const readStream = fs.createReadStream(process.argv[2]);
+const writeStream = fs.createWriteStream("output.csv");
+
+const parser = csv.parse();
+
+// define transformation
+const transformer = csv.transform(([
+    outputFrequency,
+    inputFrequency,
+    ,
+    uplinkTone,
+    downlinkTone,
+    ,
+    ,
+    lat,
+    lng,
+    callSign,
+    use,
+    ,
+    mode,
+    ,
+    ,
+    irlp
+]) => (
+    [
+        null,
+        outputFrequency,
+        inputFrequency,
+        uplinkTone,
+        downlinkTone,
+        lat,
+        lng,
+        callSign,
+        use === "OPEN",
+        mode.includes("analog"),
+        irlp
+    ]
+));
+
+const stringifier = csv.stringify();
+
+readStream.on("readable", () => {
+    let data;
+    while (data = readStream.read())
+        parser.write(data);
+});
+
+parser.on("readable", () => {
+    let data;
+    while (data = parser.read())
+        transformer.write(data);
+});
+
+transformer.on("readable", () => {
+    let data;
+    while (data = transformer.read())
+        stringifier.write(data);
+});
+
+stringifier.on("readable", () => {
+    let data;
+    while (data = stringifier.read())
+        writeStream.write(data);
+});
+
+readStream.on("end", () => {
+    // wait for stringifier to finish writing
+    while (stringifier.read())
+        ;
+
+    // update database with new data
+    const child = spawn("mysql", [`-u ${process.env.RDS_USERNAME} -p'${process.env.RDS_PASSWORD}' -h ${process.env.RDS_HOST} -P ${process.env.RDS_PORT} -D ${process.env.RDS_DB_NAME}`]);
+
+    child.stdout.on("data", msg => console.log(msg.toString()));
+    child.stderr.on("data", err => console.error(err.toString()));
+    child.on("exit", code => {
+        if (code === 0) console.log("successfully updated database");
+        else console.error("failed to update database");
+    });
+})
